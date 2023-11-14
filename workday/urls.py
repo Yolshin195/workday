8080da6d3c1a5567ea8f19135e849439e2e3930e
a.elshinv <a.elshinv@consaltica.ru>
2023-11-14 20:46:40 +0700

    version 0.3 add home page

--- workday/urls.py
+++ workday/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include("home.urls")),
+    path('workday/', include("work.urls")),
     path('admin/', admin.site.urls),
-    path('', include("work.urls")),
 ]
